6922b62d8e2eeda910f1e4d7e824a7ebecc6b8eb
Sungpil.cho <chosungpil@gmail.com>
2023-06-01 15:39:30 +0900

    refactoring

--- Project3/prime-factors.cpp
+++ Project3/prime-factors.cpp
@@ -3,17 +3,14 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i)
+	vector<int> of(int number)
 	{
 		vector<int> result = {};
-		if( i == 2)
+		if(number > 1)
 		{
-			result.push_back(2);
+			result.push_back(number);
 		}
-		if (i == 3)
-		{
-			result.push_back(3);
-		}
+
 		return result;
 	}
 	
