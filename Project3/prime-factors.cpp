df11bf78d3ea320785b02e0b18bc9b9e1728e4fc
Sungpil.cho <chosungpil@gmail.com>
2023-06-01 15:43:12 +0900

    refactoring

--- Project3/prime-factors.cpp
+++ Project3/prime-factors.cpp
@@ -10,8 +10,12 @@ public:
 		{
 			if (number == 4)
 			{
-				result.push_back(2);
-				result.push_back(2);
+				
+				while (number % 2 == 0)
+				{
+					result.push_back(2);
+					number /= 2;
+				}
 			}
 			else
 			{
